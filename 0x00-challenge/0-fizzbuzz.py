1c6adb7b3dd2209f95e9a9800f066bcacce829e7
Samuel <a2jsamuel@gmail.com>
2023-05-25 14:13:15 +0300

    fix and ad 0th file

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -19,9 +19,9 @@ def fizzbuzz(n):
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
         elif (i % 3) == 0:
+            tmp_result.append("Buzz")
+        elif (i % 5) == 0:
             tmp_result.append("Fizz")
         else:
             tmp_result.append(str(i))
